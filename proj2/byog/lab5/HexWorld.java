7b4eab472f5bf0a5f0dfca362653743338e1957d
xzc <me@mydomain.com>
2021-02-23 19:04:19 +0800

    complete lab5

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,16 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
+    public void drawline (TETile[][] tiles, TETile tile, int length, int x, int y) {
+        for (int i = 0; i < length; i++) {
+            tiles[x + i][y] = tile;
+        }
+    }
+    public void addHexagon (TETile[][] tiles, TETile tile, int size, int x, int y) {
+        for (int i = 0; i < size; i++) {
+            drawline(tiles, tile, size + 2 * i, x + size - 1 - i, y + i);
+            drawline(tiles, tile, size + 2 * i, x + size - 1 - i, y + 2 * size - 1 - i);
+        }
+    }
 }
