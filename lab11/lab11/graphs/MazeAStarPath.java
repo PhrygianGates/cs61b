175aefba0eef5b60af6372db16a9f26f70a26221
xzc <me@mydomain.com>
2021-04-03 15:35:20 +0800

    complete lab11

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import java.security.KeyPair;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  *  @author Josh Hug
  */
@@ -20,7 +24,7 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        return Math.abs(maze.toX(v) - maze.toX(t)) + Math.abs(maze.toY(v) - maze.toY(t));
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -30,9 +34,85 @@ public class MazeAStarPath extends MazeExplorer {
     }
 
     /** Performs an A star search from vertex s. */
+    private class Pair implements Comparable {
+        int item;
+        int weight;
+        Pair(int i, int w) {
+            item = i;
+            weight = w;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            if (o == null || o.getClass() != this.getClass()) {
+                throw new IllegalArgumentException();
+            }
+            Pair other = (Pair) o;
+            return weight - other.weight;
+        }
+    }
+
     private void astar(int s) {
         // TODO
+        PriorityQueue<Pair> q = new PriorityQueue<>();
+        q.add(new Pair(s, 0));
+        while (!q.isEmpty()) {
+            Pair curr = q.remove();
+            int v = curr.item;
+            if (v == t) {
+                break;
+            } else {
+                for (int w : maze.adj(v)) {
+                    if (!marked[w]) {
+                        marked[w] = true;
+                        edgeTo[w] = v;
+                        distTo[w] = distTo[v] + 1;
+                        announce();
+                        q.add(new Pair(w, distTo[w] + h(w)));
+                    }
+                }
+            }
+
+        }
     }
+    /*private class Node {
+        private int v;
+        private int priority;
+
+        public Node(int v) {
+            this.v = v;
+            this.priority = distTo[v] + h(v);
+        }
+    }
+
+    private class NodeComparator implements Comparator<Node> {
+        @Override
+        public int compare(Node o1, Node o2) {
+            return o1.priority - o2.priority;
+        }
+    }
+    private void astar(int src) {
+        PriorityQueue<Node> pq = new PriorityQueue<>(new NodeComparator());
+        Node curNode = new Node(src);
+        pq.add(curNode);
+        marked[src] = true;
+        while (!pq.isEmpty()) {
+            curNode = pq.poll();
+            for (int w : maze.adj(curNode.v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = curNode.v;
+                    distTo[w] = distTo[curNode.v] + 1;
+                    announce();
+                    if (w == t) {
+                        return;
+                    } else {
+                        pq.add(new Node(w));
+                    }
+                }
+            }
+        }
+    }*/
 
     @Override
     public void solve() {
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,24 +1,80 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
+import java.util.Random;
+
 /**
  *  @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
+
     /* Inherits public fields:
-    public int[] distTo;
-    public int[] edgeTo;
-    public boolean[] marked;
+    Maze maze;
+    int[] distTo;
+    int[] edgeTo;
+    protected boolean[] marked;
     */
+
+    private int[] cameFrom;
+    private boolean foundCircle = false;
 
     public MazeCycles(Maze m) {
         super(m);
     }
 
-    @Override
-    public void solve() {
-        // TODO: Your code here!
+    private void dfs(int s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(s);
+        while(!stack.isEmpty()) {
+            if (foundCircle) {
+                break;
+            }
+            int v = stack.pop();
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    cameFrom[w] = v;
+                    //System.out.println(w+"camefrom"+v);
+                    stack.push(w);
+                } else if (w != cameFrom[v]) { // When `w` is not the parent of `v` (circle found)
+                    int u = cameFrom[w];
+                    cameFrom[u] = w;
+                    cameFrom[w] = v;
+                    //System.out.println(w+"camefrom"+v);
+                    int cur = v;    // Reconstruct circle
+                    edgeTo[cur] = cameFrom[cur];
+                    while (cur != w) {
+                        //System.out.println(cur+"camefrom"+cameFrom[cur]);
+                        cur = cameFrom[cur];
+                        edgeTo[cur] = cameFrom[cur];
+                    }
+                    foundCircle = true;
+                    //System.out.println("here");
+                    break;
+                }
+            }
+        }
+        //System.out.println("finish");
     }
 
-    // Helper methods go here
+    @Override
+    public void solve() {
+
+        /* Serves like `edgeTo`, created because I don't want to use `edgeTo` until circle found */
+        cameFrom = new int[maze.V()];
+
+        /* Set point where circle search starts */
+        Random rand = new Random();
+        int startX = rand.nextInt(maze.N() - 1) + 1;
+        int startY = rand.nextInt(maze.N() - 1) + 1;
+        int s = maze.xyTo1D(startX, startY);
+        marked[s] = true;
+        cameFrom[s] = s;
+
+        dfs(s);
+        announce();             // Render the results of DFS
+    }
+
 }
 
